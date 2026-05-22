28c2ba44712d1d1dc890178bd5de77bb7136792c
Naga-Pixel <1andreamattioli1@gmail.com>
2026-05-22 17:19:23 +0100

    tweak(bastard pwa): 'fuck off really?' overlay only fades out (no fade-in)

--- bastard/app/sw.js
+++ bastard/app/sw.js
@@ -26,7 +26,7 @@
 'use strict';
 
 const CACHE_PREFIX = 'ub-cache-';
-const CACHE_VERSION = 'e3ac253fe50f';
+const CACHE_VERSION = '71956d702e38';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 // Replaced post-build with a JSON array of relative paths. The string
 // quotes around the placeholder keep this file as valid JS when the
